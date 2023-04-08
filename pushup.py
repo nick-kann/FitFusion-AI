ff3c111fa51f60b7ef13d76b498342463d5b988d
Mantlemoose <103068792+adu557@users.noreply.github.com>
2023-04-08 15:03:59 -0700

    updated pushup

---
+++ pushup.py
@@ -0,0 +1,98 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+def landmark_coord(landmark):
+    return np.array([landmark.x, landmark.y])
+
+def find_angle(a, b, c):
+    ba = a - b
+    bc = c - b
+
+    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    angle = np.arccos(cosine_angle)
+    return np.degrees(angle)
+
+def start():
+    # For webcam input:
+    is_up = True
+    down_count = 0
+    up_count = 0
+    rep_count = 0
+    cap = cv2.VideoCapture(0)
+    with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as pose:
+      while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+          print("Ignoring empty camera frame.")
+          # If loading a video, use 'break' instead of 'continue'.
+          continue
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(image)
+
+        # Draw the pose annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+        if results.pose_landmarks is not None:
+            # Extract pose landmarks
+            landmarks = results.pose_landmarks.landmark
+
+            # pushup test
+
+            left_shoulder = landmark_coord(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value])
+            left_elbow = landmark_coord(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value])
+            left_wrist = landmark_coord(landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+
+            left_angle = find_angle(left_shoulder, left_elbow, left_wrist)
+
+
+            right_shoulder = landmark_coord(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value])
+            right_elbow = landmark_coord(landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value])
+            right_wrist = landmark_coord(landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value])
+
+            right_angle = find_angle(right_shoulder, right_elbow, right_wrist)
+
+            if (landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].z < landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z):
+                test_angle = left_angle
+            else:
+                test_angle = right_angle
+
+
+            if (test_angle <= 90):
+                down_count += 1
+                if (down_count >= 4):
+                    up_count = 0
+                    is_up = False
+            if (test_angle >= 160):
+                up_count += 1
+                if (up_count >= 4):
+                    down_count = 0
+                    if (is_up == False):
+                        rep_count += 1
+                        print(rep_count)
+                    is_up = True
+
+
+
+        if cv2.waitKey(5) & 0xFF == 27: # esc to quit
+          break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
