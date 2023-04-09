cdfa39e60b0db1c7a2202c8c3cc0fe1a0420d1d6
Prarthan Ghosh <prarthan.ghosh@gmail.com>
2023-04-09 08:27:17 -0700

    sadfl;k

--- json_generate.py
+++ json_generate.py
@@ -18,7 +18,7 @@ def getResponse(user_prompt):
       
   return response['choices'][0]['message']['content']
     #print("Response {}: {}".format(i+1, response['choices'][0]['message']['content']))
-
+print("ge")
 
 def create():
   with open('input.txt', 'r') as f:
@@ -37,21 +37,21 @@ def create():
       #print(len(my_dict))
 
   # Print the dictionary
-  #print(my_dict)
+  print(my_dict)
 
   goal_dict = {1: "cardio", 2: "muscle toning", 3: "ab development", 4: "biscep muscle development", 5: "leg muscle development", 6: "general muscle development"}
   experience_dict = {1: "no experience", 2: "some experience", 3: "extensive experience"}
-  gym_dict = {0: "the gym", 1: "home"}
+  gym_dict = {1: "the gym", 2: "home"}
 
-  """
-  print(my_dict['weight'])
+  
+  """ print(my_dict['weight'])
   print(my_dict['height'])
   print(my_dict['goal'])
   print(goal_dict[my_dict['goal']])
   print(experience_dict[my_dict['experience']])
   print(my_dict['time_available'])
-  print(gym_dict[my_dict['location']])
-  """
+  print(my_dict['location'])"""
+  
 
   user_prompt = f"Can you please generate a 4-week, {my_dict['days_per_week']} days per week workout plan for someone whose weight is {my_dict['weight']} pounds, has a height of {my_dict['height']} inches, and whose goal for the workout is {goal_dict[my_dict['goal']]}. They have {experience_dict[my_dict['experience']]} with workouts, with {my_dict['time_available']} hours available per day to workout, and will do the workouts at {gym_dict[my_dict['location']]}. Please specify the day of the week by name. At the end please give guidance on what types of food to eat and what types of food to avoid. In your answer, format everything as a json dict file."
 
